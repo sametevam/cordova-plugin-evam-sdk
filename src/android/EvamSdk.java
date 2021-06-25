f3642513774e7a6a56e5ee9d0b842b4672348921
sametevam <69842061+sametevam@users.noreply.github.com>
2021-06-25 17:39:16 +0300

    Update EvamSdk.java

--- src/android/EvamSdk.java
+++ src/android/EvamSdk.java
@@ -8,9 +8,9 @@ public class EvamSdk extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
          try { 
-             Log.e("calis");
+          
             if (action.equals("start")) {
-             Log.e("The Dark Side of the Force is a pathway to many abilities some consider to be unnatural.");
+            
             }
              
             return true;
