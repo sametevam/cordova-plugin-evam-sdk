796d0963791cad693e56a9a92a3635a69404e125
sametevam <69842061+sametevam@users.noreply.github.com>
2021-06-28 13:11:34 +0300

    Update EvamSdk.java

--- src/android/EvamSdk.java
+++ src/android/EvamSdk.java
@@ -3,23 +3,30 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.apache.cordova.LOG;
+import org.json.JSONException;
+import java.util.Arrays;
+import android.app.Activity;
 
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PackageManager;
 
 public class EvamSdk extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-         try { 
-          
-            if (action.equals("start")) {
-            
-            }
-             
-            return true;
-         } catch (NameNotFoundException e) {
-            callbackContext.success("N/A");
-            return true;
+    // @Override
+    public void run(String action) throws JSONException {
+        if (action.equals("start")) {
+            LOG.v("", "Executing haha ");
         }
+        //  try { 
+          
+        //     if (action.equals("start")) {
+            
+        //     }
+             
+        //     return true;
+        //  } catch (NameNotFoundException e) {
+        //     callbackContext.success("N/A");
+        //     return true;
+        // }
     }
 }
