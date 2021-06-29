8404f13e791da44769a4943d5e7d42783a95ea96
sametevam <69842061+sametevam@users.noreply.github.com>
2021-06-29 14:56:42 +0300

    Update EvamSdk.java

--- src/android/EvamSdk.java
+++ src/android/EvamSdk.java
@@ -1,22 +1,25 @@
+package org.apache.cordova.evamsdk;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
-import org.json.JSONArray;
+
+// import org.apache.cordova.CallbackContext;
+// import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+// import org.json.JSONObject;
 import org.apache.cordova.LOG;
 import org.json.JSONException;
-import java.util.Arrays;
-import android.app.Activity;
+// import java.util.Arrays;
+// import android.app.Activity;
 
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.PackageManager;
+// import android.content.pm.PackageManager.NameNotFoundException;
+// import android.content.pm.PackageManager;
 
 public class EvamSdk extends CordovaPlugin {
     // @Override
-    public void run(String action) throws JSONException {
-        if (action.equals("start")) {
-            LOG.v("", "Executing haha ");
-        }
+    // public void initialize(String action) throws JSONException {
+    public boolean execute(String action) throws JSONException {
+        // if (action.equals("start")) {
+          LOG.v("", "Executing haha ");
+        // }
         //  try { 
           
         //     if (action.equals("start")) {
@@ -28,5 +31,6 @@ public class EvamSdk extends CordovaPlugin {
         //     callbackContext.success("N/A");
         //     return true;
         // }
+        return true;
     }
 }
